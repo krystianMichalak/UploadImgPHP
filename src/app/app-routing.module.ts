62e4bf6bc296e6b2b92da168746d7b85f8ff1eef
krystianMichalak <krystian.michalak@codeconcept.pl>
2020-07-07 10:31:17 +0200

    initialize project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UploadImgComponent } from "./upload-img/upload-img.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'upload', component: UploadImgComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UploadImgComponent } from './upload-img/upload-img.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadImgComponent
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
